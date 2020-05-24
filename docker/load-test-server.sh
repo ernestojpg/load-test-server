584dcd6cc2bc9758f6454d9ba538a17ab91d46fb
Ernesto J. Perez <ernestojpg@gmail.com>
2020-05-24 23:22:29 +0200

    Generate docker image for the application

---
+++ docker/load-test-server.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -z "$JAVA_OPTS" ]
+then
+    JAVA_OPTS="-Dlog4j.configurationFile=/opt/load-test-server/conf/log4j2.xml"
+fi
+
+COMMAND="java ${JAVA_OPTS} -jar /opt/load-test-server/load-test-server.jar"
+
+echo "Running Load Test Server ...."
+echo ${COMMAND}
+
+# Execute the Java application replacing the current shell process
+exec ${COMMAND}
