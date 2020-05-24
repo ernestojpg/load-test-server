48aecf14a110144ca412b13198097a79d9c11116
Ernesto J. Perez <ernestojpg@gmail.com>
2020-05-24 23:21:27 +0200

    Printing general information about the system when running the application

--- src/main/java/com/ernestojpg/loadtestserver/Main.java
+++ src/main/java/com/ernestojpg/loadtestserver/Main.java
@@ -7,6 +7,9 @@ import io.vertx.core.impl.cpu.CpuCoreSensor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 /**
  * Main.java
  *
@@ -30,9 +33,45 @@ public class Main {
     }
 
     private static void printInfo(VertxOptions vertxOptions, DeploymentOptions deploymentOptions) {
+        final DecimalFormat decimalFormat = new DecimalFormat( "#,##0.##" );
+        LOGGER.info("Operating System: {} ({})", System.getProperty("os.name"), System.getProperty("os.version"));
         LOGGER.info("Number of Processors: {}", CpuCoreSensor.availableProcessors());
+        LOGGER.info("Max memory for application: {} MB", decimalFormat.format(
+                ((float)Runtime.getRuntime().maxMemory()) / 1024 / 1024));
+        LOGGER.info("Max open files: {} (soft) , {} (hard)", getMaxOpenFilesSoft(), getMaxOpenFilesHard());
         LOGGER.info("Number of event loop threads: {}", vertxOptions.getEventLoopPoolSize());
         LOGGER.info("Deploying {} {} instances", deploymentOptions.getInstances(),
                 ServerVerticle.class.getSimpleName());
+    }
+
+    private static String getMaxOpenFilesSoft() {
+        final String osName = System.getProperty("os.name");
+        if (osName!=null && !osName.startsWith("Win")) {
+            return executeUnixCommand("ulimit -Sn");
+        }
+        return null;
+    }
+
+    private static String getMaxOpenFilesHard() {
+        final String osName = System.getProperty("os.name");
+        if (osName!=null && !osName.startsWith("Win")) {
+            return executeUnixCommand("ulimit -Hn");
+        }
+        return null;
+    }
+
+    private static String executeUnixCommand(String command) {
+        try {
+            //final Process process = Runtime.getRuntime().exec(command);
+            final Process process = new ProcessBuilder()
+                    .command("sh", "-c", command)
+                    .start();
+            try (Scanner scanner = new Scanner(process.getInputStream())) {
+                return scanner.useDelimiter("\\A").next().trim();
+            }
+        } catch (Exception e) {
+            LOGGER.warn("error executing command '{}': {}", command, e.getMessage());
+        }
+        return null;
     }
 }
